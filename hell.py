7d20a5a9a7775bd6e1db954042c09fba092ec8d8
zudeki <alex_3099@hotmail.com>
2023-10-15 15:56:37 -0600

    "primer inteento"

---
+++ hell.py
@@ -0,0 +1,36 @@
+print ('hello world') # Muestra por pantalla
+
+# program.py
+sum = 1 + 2 #Operando 
+print (sum) #Muesrta por pantalla 
+
+
+
+
+print("show this in the console") #Muestra por pantalla caracateres de tipo string aaaaa bbbb
+
+sum2 = 1+2 #3
+product = sum*2
+print(product)
+
+from datetime import date #Importa la libreria de fechas para poder usarlas
+date.today #Fecha de hoy 
+
+print(date.today()) #Muestra la fecha de hoy 
+
+print("La fecha de el dia de hoy es: " + str(date.today())) #Hay que convertir la fecha por un dato de tipo string
+
+
+print ("Bienvenido a mi programa de prueba")
+name = input ("Inserte su nombre plocs  ") #Insertar datos a traves de teclado
+print ("Saludos " + name) #Imprime los datos a traves del teclado
+
+print("Calculadora")
+Primer_numero = input("Introduzca un numero ") #Los datos entran en tipo booleano
+Segundo_numero = input("Segundo numero ") #Los datos entran en tipo boolenao
+print (" El resultado del numero es " + str(int(Primer_numero) + int(Segundo_numero))) #Para que detecte
+#Todo, se tienen que convertir los valores a entero y se adiciona si se quiere con texto el tipo string 
+
+
+
+
