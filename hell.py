a3731b6a14e79c692b69b08abb878511cefd3c79
zudeki <alex_3099@hotmail.com>
2023-10-15 16:36:50 -0600

    Creo_que_reemplaza_esto

--- hell.py
+++ hell.py
@@ -2,7 +2,7 @@ print ('hello world') # Muestra por pantalla
 
 # program.py
 sum = 1 + 2 #Operando 
-print (sum) #Muesrta por pantallaa con intento 8
+print (sum) #Muesrta por pantalla intento 9 de lo que hagooo
 
 
 
